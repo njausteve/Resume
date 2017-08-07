1d3d230637a95e1138ce51919f92757eb8088184
njausteve <njaustevedomino@gmail.com>
2017-08-08 00:16:53 +0300

    Added Dynamic interest value in about me section

--- src/app/components/aboutMe/aboutme.controller.js
+++ src/app/components/aboutMe/aboutme.controller.js
@@ -11,13 +11,21 @@
     .controller('aboutMeController', aboutMeController);
 
   /** @ngInject */
-  function aboutMeController() {
+  function aboutMeController($interval) {
+
+    var vm = this;
+
+    var myInterests = ["human behaviour", "body language", 'personal development', 'Technology', 'Data Science', 'Disruptive technologies'];
+
+    vm.interest = $interval(function () {
+
+      vm.interest = myInterests[Math.floor(Math.random() * myInterests.length)];
 
 
-
-
+    }, 3000);
 
 
   }
+
 
 })();
--- src/app/route.js
+++ src/app/route.js
@@ -3,56 +3,56 @@
  *  Resume
  * by Stephen njau(njaustevedomino@gmail.com)
  */
-(function() {
-    'use strict';
-  
-    angular.module('njausteve')
+(function () {
+        'use strict';
 
-        .config(routerConfig);
+        angular.module('njausteve')
 
-    /** @ngInject */
-    function routerConfig($stateProvider, $urlRouterProvider) {
-        $stateProvider
-        .state('about-me', {
-                url:'/about',
-                templateUrl : '/app/components/aboutMe/about.html',
-                controller: 'aboutMeController',
-                controllerAs: 'about'
-        })
-        .state('portifolio', {
-               
-                url:'/portifolio',
-                templateUrl : '',
-                controller: '',
-                controllerAs: ''
-        })
-         .state('contact', {
-               
-                url:'/contact',
-                templateUrl : '',
-                controller: '',
-                controllerAs: ''
-        })
-         .state('blog', {
-               
-                url:'/blog',
-                templateUrl : '',
-                controller: '',
-                controllerAs: ''
-        })
-        .state('resume', {
-               
-                url:'/resume',
-                // templateUrl : '',
-                // controller: '',
-                // controllerAs: ''
-        })
+                .config(routerConfig);
 
-   
-        $urlRouterProvider.otherwise('/about');
+        /** @ngInject */
+        function routerConfig($stateProvider, $urlRouterProvider) {
+                $stateProvider
+                        .state('about-me', {
+                                url: '/about',
+                                templateUrl: '/app/components/aboutMe/about.html',
+                                controller: 'aboutMeController',
+                                controllerAs: 'about'
+                        })
+                        .state('portifolio', {
 
-        // // use the HTML5 History API
-        // $locationProvider.html5Mode(true);
-    }
+                                url: '/portifolio',
+                                templateUrl: '',
+                                controller: '',
+                                controllerAs: ''
+                        })
+                        .state('contact', {
+
+                                url: '/contact',
+                                templateUrl: '',
+                                controller: '',
+                                controllerAs: ''
+                        })
+                        .state('blog', {
+
+                                url: '/blog',
+                                templateUrl: '',
+                                controller: '',
+                                controllerAs: ''
+                        })
+                        .state('resume', {
+
+                                url: '/resume',
+                                // templateUrl : '',
+                                // controller: '',
+                                // controllerAs: ''
+                        })
+
+
+                $urlRouterProvider.otherwise('/about');
+
+                // // use the HTML5 History API
+                // $locationProvider.html5Mode(true);
+        }
 
 })();
