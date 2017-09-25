6ec0e3dc186ce1f1555523b26081dc0cae4b55c9
njausteve <njaustevedomino@gmail.com>
2017-09-25 10:29:47 +0300

    included the build folder to gitignore

--- _build/app/components/blog/blog.controller.js
+++
@@ -1,6 +0,0 @@
-/**
- * (C) njausteve, 2017
- *  Resume
- * by Stephen njau(njaustevedomino@gmail.com)
- */
-!function(){"use strict";function o(){console.log("blogController loaded")}angular.module("njausteve").controller("blogController",o)}();
--- _build/app/components/contact/contact.controller.js
+++
@@ -1,6 +0,0 @@
-/**
- * (C) njausteve, 2017
- *  Resume
- * by Stephen njau(njaustevedomino@gmail.com)
- */
-!function(){"use strict";function n(n){console.log("contactController loaded"),n.getMap().then(function(n){})}n.$inject=["NgMap"],angular.module("njausteve").controller("contactController",n)}();
